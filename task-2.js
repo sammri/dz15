8128a842704832df8818626d5ac69f2d75f9fc14
sammri <ivildemort4837@gmail.com>
2025-05-13 16:50:16 +0300

    asd

--- task-2.js
+++ task-2.js
@@ -1,28 +1,2 @@
-const users = [
-    {
-    name: "samir",
-    friend: "dima",
-    },
-    {
-    name: "dima",
-    friend: "kostek",
-    },
-    {
-    name: "kostek",
-    friend: "samir",
-    },
-];
 
-const getUsersWithFriend = (users, friendName) => {
-    let friends = [];
 
-    for (const user of users) {
-        if (user.friend === friendName) {
-            friends.push(user.name);
-        }
-    }
-
-    return friends;
-};
-
-console.log(getUsersWithFriend(users, 'samir'));
--- task-3.js
+++ task-3.js
@@ -1,30 +1,4 @@
-const users = [
-    {
-        name: "Samir",
-        friends: ["Dima", "Kostek"]
-    },
-    {
-        name: "Dima",
-        friends: ["Samir"]
-    },
-    {
-        name: "Kostek",
-        friends: ["Samir", "Dima", "Alex"]
-    },
-    {
-        name: "Alex",
-        friends: []
-    }
-];
 
 
-const getNamesSortedByFriendsCount = users => {
-    const usersCopy = users.slice();
-    usersCopy.sort((a, b) => a.friends.length - b.friends.length);
-    const names = usersCopy.map(user => user.name);
-    return names;
-};
-
-console.log(getNamesSortedByFriendsCount(users));
 
   
